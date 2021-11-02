e95e1449da8402be62d030b4e59140490bfd2f10
unknown <davidobende@gmail.com>
2021-11-02 20:35:16 +0100

    new fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
+import { GlobalStyle } from "./global-styles";
 import ReviewContainer from "./components/review/ReviewContainer";
+import Footer from "./components/review/Footer";
 
 function App() {
   return (
-    <div className="app">
+    <>
+      <GlobalStyle />
       <ReviewContainer />
-    </div>
+    </>
   );
 }
 
