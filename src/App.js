69d16b8f778e9e8c2c04b6b5c549e9a1c96e1afd
unknown <davidobende@gmail.com>
2021-11-02 17:01:05 +0100

    new deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReviewContainer from "./components/review/ReviewContainer";
+
+function App() {
+  return (
+    <div className="app">
+      <ReviewContainer />
+    </div>
+  );
+}
+
+export default App;
